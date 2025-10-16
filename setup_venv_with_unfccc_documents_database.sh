017c37d0d2fcd56f0b6fa67bf8d1acd19ffaf4d7
pTarNav <ptarragonavarra@gmail.com>
2025-10-16 07:47:44 +0000

    setup venv script

---
+++ setup_venv_with_unfccc_documents_database.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e # stop if any command fails
+
+UNFCCC_DOCUMENTS_DATABASE_PATH="/home/dbs/unfccc-documents-database"
+
+# Create the python virtual environment (if it does not already exist)
+if [ ! -d ".venv" ]; then
+	echo "Creating python virtual environment in .venv..."
+	python3 -m venv ".venv"
+fi
+
+# Activate the venv
+source ".venv/bin/activate"
+
+# Install the unfccc documents database package in editiable mode
+echo "Installing unfccc-documents-database from $UNFCCC_DOCUMENTS_DATABASE_PATH in editable mode..."
+pip install -e "$UNFCCC_DOCUMENTS_DATABASE_PATH"
+
+echo "Setup complete: python virtual environment ready with unfccc-documents-database package installed."
